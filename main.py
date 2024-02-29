0c5d6cef4092277a0b1103ed0ed295123d6aa851
Eva Martin del Pico <eva.mdpico@gmail.com>
2024-02-29 12:51:19 +0100

    logs: remove bioconda

--- main.py
+++ main.py
@@ -225,7 +225,7 @@ def process_recipes():
         logger.setLevel(numeric_level)
 
         # format log message
-        formatter = logging.Formatter("%(asctime)s - %(levelname)s - bioconda - %(message)s")
+        formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
         handler.setFormatter(formatter)
 
         logger.addHandler(handler)
